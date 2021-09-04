4b045eaf1b06e88c54f63c0d2bc59e89a2af6713
maharani032 <maharanisakti015@gmail.com>
2021-09-04 14:52:35 +0700

    add git ignore

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express= require('express');
-const ejs =require('ejs');
-const app=express();
+const express = require( 'express' );
+const ejs = require( 'ejs' );
+const app = express();
 
 app.use( express.static( "public" ) );
 app.set( 'view engine', 'ejs' );
@@ -8,8 +8,12 @@ app.use( express.urlencoded( {
     extended: true
 } ) );
 
-app.get('/',(req,res)=>{
-    res.render("main")
+app.get( '/home', ( req, res ) =>
+{
+    res.render( 'home' )
+} );
+app.get('/home/',(req,res)=>{
+    res.render('home-login')
 });
 app.listen( process.env.PORT || 3000, function ()
 {
